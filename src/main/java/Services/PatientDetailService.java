ad23d08395aefd78fdb923f3a4fdf181ab8bc422
Michael <michael.leshowitz@gmail.com>
2021-04-13 21:06:11 -0400

    Made Service package, and scan for it in ApplicationSecurityConfig

--- src/main/java/Services/PatientDetailService.java
+++ src/main/java/Services/PatientDetailService.java
@@ -1,6 +1,7 @@
-package com.example.demo;
+package Services;
 
 import Repositories.PatientRepository;
+import com.example.demo.PatientPrincipal;
 import models.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
--- src/main/java/com/example/demo/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/ApplicationSecurityConfig.java
@@ -1,24 +1,20 @@
 package com.example.demo;
 
+import Services.PatientDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-public class ApplicationConfig extends WebSecurityConfigurerAdapter {
+@ComponentScan("Services")
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private PatientDetailService patientDetailService;
